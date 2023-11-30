e2a602d4fc29dc540d887105e8db302b28eebe25
hdihd9162 <53191454+hdihd9162@users.noreply.github.com>
2023-12-01 01:16:35 +0330

    add ui to code
    ui added with streamlit library

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+import streamlit as st
 # Load YOLO
 net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
 classes = []
@@ -10,52 +10,64 @@ with open("coco.names", "r") as f:
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in [net.getUnconnectedOutLayers()]]
 # Load image
-image = cv2.imread("image.jpg")
-height, width, channels = image.shape
+uploaded_files = st.file_uploader("Choose a image file", accept_multiple_files=True)
 
-# Detect objects
-blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-net.setInput(blob)
-outs = net.forward(output_layers)
+if st.button('run!!!!!!!!'):
+    for uploaded_file in uploaded_files:
+        st.write("Filename: ", uploaded_file.name)
+        st.write("Filename: ", uploaded_file)
+        with st.spinner('Wait for it...'):
+            i=str(uploaded_file.name)
+            image_list=[i]
+            for i in image_list:
+                    
+                image = cv2.imread(i)
+                height, width, channels = image.shape
 
-# Process detections
-class_ids = []
-confidences = []
-boxes = []
+                # Detect objects
+                blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+                net.setInput(blob)
+                outs = net.forward(output_layers)
 
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-        if confidence > 0.5:
-            # Object detected
-            center_x = int(detection[0] * width)
-            center_y = int(detection[1] * height)
-            w = int(detection[2] * width)
-            h = int(detection[3] * height)
+                # Process detections
+                class_ids = []
+                confidences = []
+                boxes = []
 
-            # Rectangle coordinates
-            x = int(center_x - w / 2)
-            y = int(center_y - h / 2)
+                for out in outs:
+                    for detection in out:
+                        scores = detection[5:]
+                        class_id = np.argmax(scores)
+                        confidence = scores[class_id]
+                        if confidence > 0.5:
+                            # Object detected
+                            center_x = int(detection[0] * width)
+                            center_y = int(detection[1] * height)
+                            w = int(detection[2] * width)
+                            h = int(detection[3] * height)
 
-            boxes.append([x, y, w, h])
-            confidences.append(float(confidence))
-            class_ids.append(class_id)
+                            # Rectangle coordinates
+                            x = int(center_x - w / 2)
+                            y = int(center_y - h / 2)
 
-# Apply non-max suppression
-indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+                            boxes.append([x, y, w, h])
+                            confidences.append(float(confidence))
+                            class_ids.append(class_id)
 
-# Draw bounding boxes and labels
-font = cv2.FONT_HERSHEY_SIMPLEX
-for i in range(len(boxes)):
-    if i in indexes:
-        x, y, w, h = boxes[i]
-        label = classes[class_ids[i]]
-        confidence = confidences[i]
-        color = (0, 255, 0)  # Green
-        cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-        cv2.putText(image, f"{label} {confidence:.2f}", (x, y - 10), font, 0.5, color, 2)
+                # Apply non-max suppression
+                indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 
-# Save result to file
-cv2.imwrite("result.jpg", image)
+                # Draw bounding boxes and labels
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                for i in range(len(boxes)):
+                    if i in indexes:
+                        x, y, w, h = boxes[i]
+                        label = classes[class_ids[i]]
+                        color = (0, 0, 0)  # Green
+                        cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+                        cv2.putText(image, f"{label} {confidence:.2f}", (x, y - 10), font, 0.5, color, 2)
+
+                # Save result to file
+                cv2.imwrite("result.jpg", image)
+                st.image(image, caption='detected image')
+                st.success('Done!')
